e4221c33b71001cb91a25c20927cf92aa0465159
stupid_coder_jyy <stupid_coder_jyy@163.com>
2023-08-04 22:48:09 +0800

    解析正则表达式生成NFA的测试Main方法

--- src/com/stupidcoder/cc/Main.java
+++ src/com/stupidcoder/cc/Main.java
@@ -1,10 +1,14 @@
 package com.stupidcoder.cc;
 
+import com.stupidcoder.cc.lex.NFA;
+import com.stupidcoder.cc.lex.NFANode;
 import com.stupidcoder.cc.lex.NFARegexParser;
 import com.stupidcoder.cc.util.input.StringInput;
 
+import java.util.Stack;
+
 public class Main {
     public static void main(String[] args) {
-        NFARegexParser.parse(new StringInput("\\**"));
+        NFARegexParser.parse(new StringInput("[0-9]+|0[xX][0-9A-Fa-f]+")).print();
     }
 }
